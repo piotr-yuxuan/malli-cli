e09d4312c9094dce7e5e8cf3c8db82d9a2a665c1
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-06-25 18:27:35 +0100

    Update maven coordinate group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject piotr-yuxuan/malli-cli (-> "./resources/malli-cli.version" slurp .trim)
+(defproject com.github.piotr-yuxuan/malli-cli (-> "./resources/malli-cli.version" slurp .trim)
   :description "A Clojure map which implements java.io.Closeable"
   :url "https://github.com/piotr-yuxuan/malli-cli"
   :license {:name "European Union Public License 1.2 or later"
