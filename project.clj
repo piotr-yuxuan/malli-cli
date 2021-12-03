05259dc54632611a21a1b6dbbb68daa78ff77bdd
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-12-03 17:48:11 +0000

    Update description and GitHub topics

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.github.piotr-yuxuan/malli-cli (-> "./resources/malli-cli.version" slurp .trim)
-  :description "A Clojure map which implements java.io.Closeable"
+  :description "Command-line interface from the comfort of a metosin/malli schema"
   :url "https://github.com/piotr-yuxuan/malli-cli"
   :license {:name "European Union Public License 1.2 or later"
             :url "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
@@ -10,9 +10,8 @@
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
   :dependencies []
-  :profiles {:github {:github/topics ["map" "clojure" "state-management" "component"
-                                      "state" "mount" "integrant" "closeable" "deps-edn"
-                                      "tools-cli" "with-open" "clojure-maps"]}
+  :profiles {:github {:github/topics ["cli" "clojure" "command-line" "cli-app"
+                                      "command-line-tool" "args-parser" "malli"]}
              :provided {:dependencies [[org.clojure/clojure "1.10.3"]
                                        [metosin/malli "0.6.2"]]}
              :dev {:global-vars {*warn-on-reflection* true}
