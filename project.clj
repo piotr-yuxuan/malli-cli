054186bb14fcd856383a8f76bcc8b5b86842df81
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-04-30 18:37:28 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                                       "bb" "graalvm" "command-line-parser"]
                       :github/private? false}
              :provided {:dependencies [[org.clojure/clojure "1.12.0-alpha10"]
-                                       [metosin/malli "0.16.0"]]}
+                                       [metosin/malli "0.16.1"]]}
              :dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[camel-snake-kebab "0.4.3"]]
                    :jvm-opts ["-Dclojure.compiler.direct-linking=false"]}
