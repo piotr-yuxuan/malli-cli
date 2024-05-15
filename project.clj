f8425e8e309f02d07a7d7453549fe38330fe56bb
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-05-15 18:36:28 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                    :dependencies [[camel-snake-kebab "0.4.3"]]
                    :jvm-opts ["-Dclojure.compiler.direct-linking=false"]}
              :jar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.89.1380"]]}]}
+             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.90.1383"]]}]}
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/WALTER_CLOJARS_USERNAME
